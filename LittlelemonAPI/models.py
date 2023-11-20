b11076fd7e9fb636f983360d344c46eaf7297c9c
Yi <chenyiapp@yahoo.com>
2023-11-19 16:30:36 -0800

    models

--- LittlelemonAPI/models.py
+++ LittlelemonAPI/models.py
@@ -28,3 +28,13 @@ class Order(models.Model):
     status=models.BooleanField(db_index=True,default=0)
     total=models.DecimalField(max_digits=6,decimal_places=2)
     date=models.DateField(db_index=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+
+    class Meta():
+        unique_together = ('order','menuitem')
