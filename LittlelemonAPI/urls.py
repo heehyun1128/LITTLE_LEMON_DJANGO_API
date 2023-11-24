fb1dbda03a6a96e7b8446b3ea74997743c476923
Yi <chenyiapp@yahoo.com>
2023-11-23 16:28:12 -0800

    ..

--- LittlelemonAPI/urls.py
+++ LittlelemonAPI/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path,include
-from .views import menu_item_list_view,menu_item_detail_view,all_managers_view,secret,manage_delivery_crew_view,delivery_crew_remove_view,cart_view,order_view,single_order_view,sort_menu_by_price
+from .views import menu_item_list_view,menu_item_detail_view,category_view, all_managers_view,manage_delivery_crew_view,delivery_crew_remove_view,cart_view,order_view,single_order_view,sort_menu_by_price
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu-items/', menu_item_list_view, name='menu-item-list'),
-    path('menu-items/sort=price',sort_menu_by_price , name='menu-item-sort_by_price'),
+    path('menu-items/sort=price',sort_menu_by_price , name='menu-item-sort-by-price'),
     path('menu-items/<int:pk>', menu_item_detail_view, name='menu-items'),
     path('groups/manager/users', all_managers_view, name='manager_list'),
     path('groups/delivery-crew/users', manage_delivery_crew_view, name='delivery_crew'),
     path('groups/delivery-crew/users/<int:pk>', delivery_crew_remove_view, name='delivery_crew_remove'),
+    path('category', category_view, name='category_view'),
     path('cart/menu-items', cart_view, name='cart_view'),
     path('orders', order_view, name='order_view'),
     path('orders/<int:pk>', single_order_view, name='single_order_view'),
